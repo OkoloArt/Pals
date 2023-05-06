e237868b8a14b66bd39de0f2327f3fde4c1b41b7
Arthur Okolo <Okoloarthur4@gmail.com>
2023-05-06 21:03:34 +0100

    feat: Testing loading image status into status recyclerview
    *

--- app/src/main/java/com/example/helloworld/ui/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/helloworld/ui/fragments/ChatFragment.kt
@@ -4,25 +4,20 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.helloworld.adapter.ChatAdapter
-import com.example.helloworld.adapter.ContactAdapter
 import com.example.helloworld.adapter.StatusAdapter
 import com.example.helloworld.common.Constants.CHAT_LIST
 import com.example.helloworld.common.Constants.USERS
-import com.example.helloworld.common.utils.AppUtil
 import com.example.helloworld.common.utils.AppUtil.Companion.getMobileContacts
 import com.example.helloworld.common.utils.FirebaseUtils.firebaseAuth
 import com.example.helloworld.common.utils.FirebaseUtils.firebaseDatabase
@@ -36,8 +31,8 @@ import com.example.helloworld.ui.viewmodel.ContactViewModel
 import com.example.helloworld.ui.viewmodel.ProfileViewModel
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import java.util.concurrent.CompletableFuture
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -55,10 +50,10 @@ class ChatFragment : Fragment() {
     private val contactViewModel : ContactViewModel by viewModels()
     private lateinit var chatAdapter: ChatAdapter
     private lateinit var statusAdapter: StatusAdapter
-    private lateinit var  userStatus: MutableSet<UserStatus>
-    private lateinit var  imageStatus: MutableSet<ImageStatus>
+    private var user : User? = null
 
     private var imageUri: Uri? = null
+    private var imageString : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -85,8 +80,12 @@ class ChatFragment : Fragment() {
         readChat()
         setUpStatusRecyclerview()
 
-        profileViewModel.getUser().observe(viewLifecycleOwner){ user ->
-            bindDetail(user)
+        profileViewModel.getUser().observe(viewLifecycleOwner){ currentUser ->
+            bindDetail(currentUser)
+            user = currentUser
+        }
+        binding.status.setOnClickListener{
+            uploadImageStatus()
         }
     }
 
@@ -176,17 +175,27 @@ class ChatFragment : Fragment() {
             imageUri = result.data?.data
             if (imageUri == null) return@registerForActivityResult
             requireActivity().contentResolver.takePersistableUriPermission(imageUri!! , Intent.FLAG_GRANT_READ_URI_PERMISSION)
-    //        binding.profileImage.setImageURI(imageUri)
+            imageString = imageUri.toString()
+            binding.status.setImageURI(imageUri)
+            setImageStatus(user!!)
 //            imageUri?.let { lifecycleScope.launch { userPreferences.saveProfilePic(it.toString()) }}
         }
     }
 
-    private fun setImageStatus() {
+    private fun uploadImageStatus() {
         val gallery = Intent(Intent.ACTION_OPEN_DOCUMENT , MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         gallery.flags = (Intent.FLAG_GRANT_READ_URI_PERMISSION
                 or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
                 or Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
         pickImageLauncher.launch(gallery)
+    }
+
+    private fun setImageStatus(user: User){
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd" , Locale.getDefault())
+        val currentDate = dateFormat.format(Date())
+        val newImage = ImageStatus(imageString!! , currentDate)
+        user.imageStatus!!.add(newImage)
+        profileViewModel.updateUser(user)
     }
 
 }
